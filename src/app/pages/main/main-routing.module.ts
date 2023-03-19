8a0d06063cba6eb4c04a1c830f6fb00e77a83e9a
lenoox <62141973+lenoox@users.noreply.github.com>
2023-03-19 18:14:16 +0100

    add about page (#15)
    * add about page
    
    * fix pagination in table

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
-import { AuthGuard } from '../../core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -22,6 +21,11 @@ const routes: Routes = [
           import('../candidates/candidates.module').then(
             m => m.CandidatesModule
           ),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('../about/about.module').then(m => m.AboutModule),
       },
     ],
   },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,8 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MainModule } from './main/main.module';
 import { LoginModule } from './login/login.module';
+import { AboutModule } from './about/about.module';
 
-const MODULES = [CommonModule, PagesRoutingModule, MainModule, LoginModule];
+const MODULES = [
+  CommonModule,
+  PagesRoutingModule,
+  MainModule,
+  LoginModule,
+  AboutModule,
+];
 @NgModule({
   declarations: [],
   imports: [...MODULES],
