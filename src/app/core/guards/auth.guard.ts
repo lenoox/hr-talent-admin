24a315cb4b0414935f8c68a2378d88c43cc32bae
lenoox <62141973+lenoox@users.noreply.github.com>
2023-05-20 20:13:41 +0200

    fix login guard (#18)

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -4,41 +4,42 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
+  UrlTree,
 } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
-import { catchError, mergeMap, Observable, of, tap } from 'rxjs';
+import { catchError, Observable, of, map } from 'rxjs';
 import { UserState } from '../state/user/user.state';
 import { GetUser, Reset } from '../state/user/user.action';
+import { UserResponse } from '../state/user/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  @Select(UserState.isLogged) userState$!: Observable<boolean>;
+  @Select(UserState.isLogged) isLogged$!: Observable<boolean>;
+  @Select(UserState.getLoggedUser) loggedUser$!: Observable<UserResponse>;
   constructor(private router: Router, private store: Store) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.store.dispatch(new GetUser()).pipe(
-      catchError(() => {
-        this.store.dispatch(new Reset());
-        return this.isLogged(false);
-      }),
-      mergeMap((isLogged: boolean) => {
-        return this.isLogged(isLogged);
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.store.dispatch(new GetUser()).pipe(catchError(error => of(null)));
+    return this.loggedUser$.pipe(
+      map(loggedUser => {
+        return this.isLogged(!!loggedUser?.email);
       })
     );
   }
-  redirect(): Observable<boolean> {
-    this.router.navigate(['/login/user']);
-    return of(false);
-  }
-  isLogged(isLogged: boolean): Observable<boolean> {
+  isLogged(isLogged: boolean): boolean {
     if (isLogged) {
-      return of(true);
+      return true;
     } else {
-      return this.redirect();
+      this.router.navigateByUrl('/login/user');
+      return false;
     }
   }
 }
